ba1178afea66de899c6224c3d94e5379e40482b9
adbik <077bei015.bikash@pcampus.edu.np>
2025-08-22 16:02:22 +0545

    random change

--- src/routes/testRoutes.js
+++ src/routes/testRoutes.js
@@ -4,7 +4,7 @@ const test = express.Router();
 
 test.get('/testAPI', async (req, res) => {
     try{
-        res.status(201).json({message:'iLearn backend connected successfully. it works !'});
+        res.status(201).json({message:'bubsies'});
     }
     catch(err){
         res.status(500).json({error: "Failed to connect"});
